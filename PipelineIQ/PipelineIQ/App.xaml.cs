18af33745241b6690e13bd2a50320c3ce4d69f46
Juan Carlos Arrieta Rincón <juan.arrieta.r@outlook.com>
2025-02-04 21:23:09 -0400

    Initial Process

---
+++ PipelineIQ/PipelineIQ/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace PipelineIQ
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new NavigationPage(new MainPage()));
+            return window;
+        }
+    }
+}
---
+++ PipelineIQ/PipelineIQ/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PipelineIQ
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
